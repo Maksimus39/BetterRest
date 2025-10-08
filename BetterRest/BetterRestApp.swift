f9229110070b296e9f3fa7c582b5e6f353dd2fb1
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-07 17:19:12 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Максим Минаков on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
